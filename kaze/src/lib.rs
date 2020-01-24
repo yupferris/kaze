26569520135c0666e1aa4b46b925fff2ce4e65a7
ferris <yupferris@gmail.com>
2020-01-24 22:58:43 +0100

    Barebones module-level doc, small fixups
    Soooo much more could be written here, but I want to flesh it out more later once I have a better idea of how this is used and can provide more complete/better descriptions and examples.

--- kaze/src/lib.rs
+++ kaze/src/lib.rs
@@ -1,12 +1,43 @@
-//! An HDL embedded in Rust.
+//! An [HDL](https://en.wikipedia.org/wiki/Hardware_description_language) embedded in [Rust](https://www.rust-lang.org/).
 //!
-//! kaze has two primary goals:
-//! - this is one TODO
-//! - this is the other one TODO
+//! kaze provides an API to describe [`Module`]s composed of [`Signal`]s, which can then be used to generate [Rust simulator code](sim/fn.generate.html) or [verilog modules](verilog/fn.generate.html).
 //!
-//! kaze's primary construct is the [`Module`]. Similar to Verilog/VHDL, a [`Module`] represents a blueprint or template for a given hardware module. [`Module`]s can be used to generate code directly, or be instantiated in other [`Module`]s.
+//! kaze's API is designed to be as minimal as possible while still being expressive.
+//! It's designed to keep the user from being able to describe buggy or incorrect hardware as much as possible.
+//! This enables a user to hack on designs fearlessly, while the API and generators ensure that these designs are sound.
+//!
+//! # Usage
+//!
+//! ```toml
+//! [dependencies]
+//! kaze = "0.1"
+//! ```
+//!
+//! # Examples
+//!
+//! ```rust
+//! # fn main() -> std::io::Result<()> {
+//! use kaze::*;
+//!
+//! // Create a context, which will contain our module(s)
+//! let c = Context::new();
+//!
+//! // Create a module
+//! let inverter = c.module("inverter");
+//! let i = inverter.input("i", 1); // 1-bit input
+//! inverter.output("o", !i); // 1-bit output, representing inverted input
+//!
+//! // Generate Rust simulator code
+//! sim::generate(inverter, std::io::stdout())?;
+//!
+//! // Generate verilog code
+//! verilog::generate(inverter, std::io::stdout())?;
+//! # Ok(())
+//! # }
+//! ```
 //!
 //! [`Module`]: ./graph/struct.Module.html
+//! [`Signal`]: ./graph/struct.Signal.html
 
 mod code_writer;
 mod graph;
