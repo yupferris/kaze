62ed1de43dfeb3d232e2bb25b288601cf088ec52
ferris <yupferris@gmail.com>
2020-01-24 23:17:28 +0100

    Barebones doc for kaze_sugar
    Again, at some point I could write a lot more, especially on motivation (particularly when several signals should rely on a given conditional, and perhaps how that typically looks with functional programming), but I'm getting impatient.

--- kaze/src/graph/sugar.rs
+++ kaze/src/graph/sugar.rs
@@ -1,3 +1,29 @@
+/// **UNSTABLE:** Provides a convenient way to write conditional combinational logic.
+///
+/// # Panics
+///
+/// Since this macro rewrites the referenced variable assignments using [`mux`], any panic conditions from that method apply to the generated code as well.
+///
+/// # Examples
+///
+/// ```
+/// use kaze::*;
+///
+/// let c = Context::new();
+///
+/// let m = c.module("my_module");
+/// let i = m.input("i", 1);
+/// let invert = m.input("invert", 1);
+/// let mut o = i;
+/// kaze_sugar! {
+///     if (invert) {
+///         o = !o; // Equivalent to `o = invert.mux(!o, o);`
+///     }
+/// }
+/// m.output("o", o);
+/// ```
+///
+/// [`mux`]: ./struct.Signal.html#method.mux
 #[macro_export(local_inner_macros)]
 macro_rules! kaze_sugar {
     ($($contents:tt)*) => {
